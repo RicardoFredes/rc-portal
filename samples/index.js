2c032ba26396dba695385a6fa9be876a6860e2c0
Ricard Fredes <ricardofredes@gmail.com>
2019-12-04 18:40:11 -0300

    update samples

--- samples/index.js
+++ samples/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import portal, { Modal } from '../dist/index.js';
 
 class App extends React.PureComponent {
+  handleClick() {
+    const { close, parent } = rcPortal(MyPortalComponent);
+    console.log(parent); // show the parent element
+    setTimeout(close, 5000); // close the portal
+  }
+
   render() {
     return (
       <div>
@@ -10,6 +16,9 @@ class App extends React.PureComponent {
           onClick={() => portal(MyModal, { id: 'modal', title: 'My Modal' })}
         >
           Open Modal
+        </button>
+        <button onClick={this.handleClick.bind(this)}>
+          Modal auto close after 5s
         </button>
       </div>
     );
