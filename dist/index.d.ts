a34d5a4a1641a71352fa719cb624628f81480fb7
Ricardo Fredes <ricardofredes@gmail.com>
2021-01-06 14:31:28 -0300

    fix: ensure export ComponentsProps

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,5 @@ import rcPortal from './rcPortal';
 export { RcPortal } from './rcPortal';
 export { Modal } from './components/Modal';
 export { Notification } from './components/Notification';
+export { ComponentsProps } from './components/ComponentProps';
 export default rcPortal;
